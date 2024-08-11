7b322330629f4126bb6305d9d8d23f06d1978ab8
AmirxonEngineer <amirshohxaitov98@gmail.com>
2024-08-11 08:54:55 +0500

    add dars

---
+++ 1-dars/data.js
@@ -0,0 +1,37 @@
+"use strict"
+
+
+//Number
+let number = 5.6; // Data number
+console.log(-8 / 0); // Infinity
+console.log("amir" * 8); // Nan
+
+
+// String
+ const clientName = 'Amir';
+
+// Boolean
+const isMarried = true;
+const isMarriedNol = false;
+
+// null
+console.log(clientAge);  // yo'q o'zgaruvchi
+
+// undefined
+let und;
+console.log(und); // o'zgaruvchida ma'lumot yuq
+
+// Object
+
+const amir = {
+  // key: value
+  age: 26,
+  jacket: "qora",
+  isLong: true,
+};
+
+console.log(amir.age);
+
+// array
+const colors = ["red" , "green"];
+console.log(colors[0]);
---
+++ 2-dars/muloqot.js
@@ -0,0 +1,9 @@
+"use strict"  // Qatiiy rejim
+
+// alert("hello world");
+
+// const place = confirm("Are you here");
+
+// console.log(place);
+
+const currentPlace = prompt("Where are you", "amir");
