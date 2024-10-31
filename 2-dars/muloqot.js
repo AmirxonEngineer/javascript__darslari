757f4a97005f7367e5354e8c20d97cb4dac048c0
AmirxonEngineer <amirshohxaitov98@gamil.com>
2024-10-31 10:57:31 +0500

    deleted

--- 2-dars/muloqot.js
+++
@@ -1,9 +0,0 @@
-"use strict"  // Qatiiy rejim
-
-// alert("hello world");
-
-// const place = confirm("Are you here");
-
-// console.log(place);
-
-const currentPlace = prompt("Where are you", "amir");
